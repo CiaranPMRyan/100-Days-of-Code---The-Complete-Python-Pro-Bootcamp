53346d9b9fac2ded62d08f6c1d4b9e1a6e8c5f0c
CiaranPMRyan <ciaranpmryan@gmail.com>
2025-08-05 09:52:47 +0200

    Add new project: day-29-start

---
+++ day-29-start/main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=2, highlightbackground="black")
+bg_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=bg_image)
+canvas.pack()
+
+
+
+
+window.mainloop()
