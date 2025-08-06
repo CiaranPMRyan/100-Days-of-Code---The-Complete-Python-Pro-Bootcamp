1f149841e2f80a9934271cdd39eda8f6c2038a90
Ciaran Ryan <ciaranpmryan@gmail.com>
2025-08-06 10:52:42 +0200

    Day 29 interface layout

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-29-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-29-start/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+from pandas.core.computation.align import align_terms
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -10,12 +12,43 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
+# Grid layout will be as follows:
+# logo in col 1, row 0
+
+# Logo graphic
 canvas = Canvas(width=200, height=200, highlightthickness=2, highlightbackground="black")
 bg_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=bg_image)
-canvas.pack()
+canvas.grid(column=1, row=0)
 
+# Label1, col 0, row 1,
+label_web = Label(text="Website:")
+label_web.grid(column=0, row=1)
 
+#Entry1 col 1, row 1, colspan 2
 
+entry_web = Entry(width=35)
+entry_web.grid(column=1, row=1, columnspan=2)
+
+# Label2, col 0, row 2,
+label_user = Label(text="Email/Username:")
+label_user.grid(column=0, row=2)
+
+# Entry2 col 1, row 2, colspan 2
+entry_user = Entry(width=35)
+entry_user.grid(column=1, row=2, columnspan=2)
+
+# Label3, col 0, row 3,
+label_password = Label(text="Password:")
+label_password.grid(column=0, row=3)
+
+# Entry3 col 1, row 3, colspan 2, smaller width
+entry_password = Entry(width=25, justify="left")
+entry_password.grid(column=1, row=3)
+
+# Button 1 col 3, row 3
+button_gen = Button(text="Generate Password")
+button_gen.grid(column=2, row=3)
+# Button 2 col 1, row 4, colspan 2
 
 window.mainloop()
