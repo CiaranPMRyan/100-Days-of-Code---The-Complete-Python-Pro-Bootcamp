7812a1e2cfd68416faab3318aac0f2a8162a6eaf
Ciaran Ryan <ciaranpmryan@gmail.com>
2025-08-07 09:28:39 +0200

    Add function to save data to text file

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-29-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-29-start/main.py
@@ -6,13 +6,23 @@ from pandas.core.computation.align import align_terms
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    web = entry_web.get()
+    user = entry_user.get()
+    password = entry_password.get()
+    #print(web)
+    with open("data.txt", "a") as f:
+         f.write(f"{web} | {user} | {password}\n")
+
+    entry_web.delete(0, END)
+    entry_password.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-# Grid layout will be as follows:
 # logo in col 1, row 0
 
 # Logo graphic
@@ -29,6 +39,7 @@ label_web.grid(column=0, row=1)
 
 entry_web = Entry(width=35)
 entry_web.grid(column=1, row=1, columnspan=2)
+entry_web.focus()
 
 # Label2, col 0, row 2,
 label_user = Label(text="Email/Username:")
@@ -37,18 +48,23 @@ label_user.grid(column=0, row=2)
 # Entry2 col 1, row 2, colspan 2
 entry_user = Entry(width=35)
 entry_user.grid(column=1, row=2, columnspan=2)
+entry_user.insert(0, "myemail@email.com")
+
 
 # Label3, col 0, row 3,
 label_password = Label(text="Password:")
 label_password.grid(column=0, row=3)
 
 # Entry3 col 1, row 3, colspan 2, smaller width
-entry_password = Entry(width=25, justify="left")
+entry_password = Entry(width=20, show="*")
 entry_password.grid(column=1, row=3)
 
 # Button 1 col 3, row 3
 button_gen = Button(text="Generate Password")
 button_gen.grid(column=2, row=3)
+
 # Button 2 col 1, row 4, colspan 2
+button_add = Button(text="Add", width=30, command=save)
+button_add.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
