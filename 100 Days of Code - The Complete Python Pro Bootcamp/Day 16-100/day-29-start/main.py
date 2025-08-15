6c5579eef6b7207e77ba6ac04db1a297d1f261ec
Ciaran Ryan <ciaranpmryan@gmail.com>
2025-08-15 13:56:45 +0200

    Add search function including error handling

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-29-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-29-start/main.py
@@ -6,11 +6,9 @@ import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
 
 def pass_generator():
 
@@ -59,6 +57,25 @@ def save():
             entry_web.delete(0, END)
             entry_password.delete(0, END)
 
+# ---------------------------- SEARCH FUNCTION ------------------------------- #
+
+def find_password():
+    web = entry_web.get()
+
+    try:
+        with open("data.json", "r") as f:  # Open the file in read mode
+            data = json.load(f)
+            output_user = data[web]["user"]
+            output_password = data[web]["password"]
+            messagebox.showinfo(title=web, message=f"Email: {output_user} \n Password: {output_password}")
+            entry_web.delete(0, END)
+    except KeyError:
+        messagebox.showerror(title="Error", message="No details for the website exist. Please search again.")
+        entry_web.delete(0, END)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No data file found")
+
+# Angela says to only use the try except when something could rarely happen, or you can't do it easily with an if else
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -74,39 +91,38 @@ bg_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=bg_image)
 canvas.grid(column=1, row=0)
 
-# Label1, col 0, row 1,
+# ---- Label setup ---- #
+
 label_web = Label(text="Website:")
 label_web.grid(column=0, row=1, sticky="w")
 
-#Entry1 col 1, row 1, colspan 2
-
-entry_web = Entry(width=30)
-entry_web.grid(column=1, row=1, columnspan=2, sticky="we")
-entry_web.focus()
-
-# Label2, col 0, row 2,
 label_user = Label(text="Email/Username:")
 label_user.grid(column=0, row=2, sticky="w")
 
-# Entry2 col 1, row 2, colspan 2
+label_password = Label(text="Password:")
+label_password.grid(column=0, row=3, sticky="w")
+
+# ---- Entry setup ---- #
+
+entry_web = Entry(width=30)
+entry_web.grid(column=1, row=1, sticky="we")
+entry_web.focus()
+
 entry_user = Entry(width=30)
 entry_user.grid(column=1, row=2, columnspan=2, sticky="we")
 entry_user.insert(0, "myemail@email.com")
 
-
-# Label3, col 0, row 3,
-label_password = Label(text="Password:")
-label_password.grid(column=0, row=3, sticky="w")
-
-# Entry3 col 1, row 3, colspan 2, smaller width
 entry_password = Entry(width=15, show="*")
 entry_password.grid(column=1, row=3, sticky="we")
 
-# Button 1 col 3, row 3
+# ---- Buttons ---- #
+
+button_search = Button(text="Search", command=find_password, width=15)
+button_search.grid(column=2, row=1)
+
 button_gen = Button(text="Generate Password", command=pass_generator)
 button_gen.grid(column=2, row=3)
 
-# Button 2 col 1, row 4, colspan 2
 button_add = Button(text="Add", width=44, command=save)
 button_add.grid(column=1, row=4, columnspan=2)
 
