33959f70913068b2f813fb9adeebe9d48fa9f28f
Ciaran Ryan <ciaranpmryan@gmail.com>
2025-08-20 22:37:53 +0200

    Start of list addition to change language

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-31/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-31/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from xml.dom.xmlbuilder import Options
+
 import pandas as pd
 import random
 
@@ -8,20 +10,16 @@ CAN_Y = 526
 
 FONT_NAME = "arial"
 
-# ---------- Pandas Logic ---------- #
-
-data = pd.read_csv("./data/french_words.csv")
-data.to_dict(orient="records")
-
 def word_generator():
     """ Picks a random word pairing from the spreadsheet """
+    global lang
     pairing = data.iloc[random.randrange(len(data) - 1)]
-    foreign_word = pairing.French
-    english_word = pairing.English
-    canvas.itemconfig(guess_word, text=pairing.French)
-
+    foreign_word = pairing[lang]
+    english_word = pairing["English"]
+    canvas.itemconfig(guess_word, text=pairing[lang])
 
 #print(f"{pairing.English} - {pairing.French}")
+
 
 # ---------- UI SETUP ---------- #
 
@@ -38,6 +36,21 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 language = canvas.create_text(CAN_X / 2, CAN_Y / 4, text="", fill="black", font=(FONT_NAME, 30, "italic"))
 guess_word = canvas.create_text(CAN_X / 2, CAN_Y / 2, text="", fill="black", font=(FONT_NAME, 60, "bold"))
+
+# List Creation #
+
+lang_list = ["Dutch", "French", "German"] # List to populate the box
+lang_sel = StringVar(window)
+lang_sel.set(lang_list[0])
+lang_sel_dropdown = OptionMenu(window, lang_sel, *lang_list)
+lang_sel_dropdown.grid(row=0, column=0)
+
+# ---------- Pandas Logic ---------- #
+
+lang = lang_sel.get()
+
+data = pd.read_csv(f"./data/{lang}_words.csv")
+data.to_dict(orient="records")
 
 # Buttons #
 
