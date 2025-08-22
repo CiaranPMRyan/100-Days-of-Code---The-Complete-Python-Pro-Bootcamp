c584fecf0666a54f4c58057921a83bdd8ae641ca
Ciaran Ryan <ciaranpmryan@gmail.com>
2025-08-22 10:18:29 +0200

    Create dropdown list for language selection.

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-31/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-31/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.ttk import Combobox
 from xml.dom.xmlbuilder import Options
 
 import pandas as pd
@@ -12,13 +13,18 @@ FONT_NAME = "arial"
 
 def word_generator():
     """ Picks a random word pairing from the spreadsheet """
-    global lang
-    pairing = data.iloc[random.randrange(len(data) - 1)]
-    foreign_word = pairing[lang]
-    english_word = pairing["English"]
-    canvas.itemconfig(guess_word, text=pairing[lang])
+    # ---------- Pandas Logic ---------- #
 
-#print(f"{pairing.English} - {pairing.French}")
+    lang = combo.get() # Gets the string from the dropdown list
+
+    data = pd.read_csv(f"./data/{lang}_words.csv")
+    data.to_dict(orient="records")
+
+    pairing = data.iloc[random.randrange(len(data) - 1)]
+    foreign_word = pairing[f"{lang}"]
+    english_word = pairing["English"]
+    canvas.itemconfig(language, text=f"{lang}")
+    canvas.itemconfig(guess_word, text=pairing[f"{lang}"])
 
 
 # ---------- UI SETUP ---------- #
@@ -30,37 +36,35 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Flash Card #
 
 canvas = Canvas(width=CAN_X, height=CAN_Y, highlightthickness=0, bg=BACKGROUND_COLOR)
-bg_image = PhotoImage(file="./images/card_front.png")
-canvas.create_image(CAN_X / 2, CAN_Y / 2, image=bg_image)
-canvas.grid(row=0, column=0, columnspan=2)
+front_image = PhotoImage(file="./images/card_front.png")
+back_image = PhotoImage(file="./images/card_back.png")
+canvas.create_image(CAN_X / 2, CAN_Y / 2, image=front_image)
+canvas.grid(row=1, column=0, columnspan=2)
 
 language = canvas.create_text(CAN_X / 2, CAN_Y / 4, text="", fill="black", font=(FONT_NAME, 30, "italic"))
 guess_word = canvas.create_text(CAN_X / 2, CAN_Y / 2, text="", fill="black", font=(FONT_NAME, 60, "bold"))
 
 # List Creation #
 
-lang_list = ["Dutch", "French", "German"] # List to populate the box
-lang_sel = StringVar(window)
-lang_sel.set(lang_list[0])
-lang_sel_dropdown = OptionMenu(window, lang_sel, *lang_list)
-lang_sel_dropdown.grid(row=0, column=0)
+label = Label(text="Please choose your langauge: ", font=(FONT_NAME, 10, "bold"))
+label.config(bg=BACKGROUND_COLOR)
+label.grid(row=0, column=0)
 
-# ---------- Pandas Logic ---------- #
+combo = Combobox(state="readonly", values=["Dutch", "French", "German"])
+combo.current(0) # Sets a default to Dutch so it can pass in something at the start
+combo.bind("<<ComboboxSelected>>", lambda _ : word_generator()) # Makes the new option change straight away
+combo.grid(row=0, column=1)
 
-lang = lang_sel.get()
-
-data = pd.read_csv(f"./data/{lang}_words.csv")
-data.to_dict(orient="records")
 
 # Buttons #
 
 wrong_image = PhotoImage(file="./images/wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=word_generator)
-wrong_button.grid(column=0, row=1)
+wrong_button.grid(column=0, row=2)
 
 right_image = PhotoImage(file="./images/right.png")
 right_button = Button(image=right_image, highlightthickness=0, command=word_generator)
-right_button.grid(column=1, row=1)
+right_button.grid(column=1, row=2)
 
 word_generator()
 
