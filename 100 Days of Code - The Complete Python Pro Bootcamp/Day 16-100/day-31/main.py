65625f0b1807cb98d11562eb9594e16f1efed7ac
Ciaran Ryan <ciaranpmryan@gmail.com>
2025-09-01 11:56:20 +0200

    Start of word tracking. It's a little more complicated due to having multiple languages.
    Includes notes on how to tackle the problem.

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-31/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/day-31/main.py
@@ -19,27 +19,44 @@ def set_language():
     lang = combo.get()  # Gets the string from the dropdown list
 
     data = pd.read_csv(f"./data/{lang}_words.csv")
+
+    # NOTE for edits - Create a base data file which is the one with the language. Then create a to_learn CSV which is populated with that data. Push that file into the to_learn variable
+    # in the correct() function, update the to_learn CSV each time so when it is called here, the words are not in it.
+
     to_learn = data.to_dict(orient="records")
+    return lang
 
 def word_generator():
     global lang, current_card, to_learn, flip_timer
-    window.after_cancel(flip_timer)
+    #window.after_cancel(flip_timer)
     set_language()
 
-    """ Picks a random word pairing from the spreadsheet """
+    """ Picks a random word pairing from the to_learn list """
     current_card = random.choice(to_learn)
+    print(to_learn)
 
     canvas.itemconfig(canvas_image, image=front_image)
     canvas.itemconfig(language, text=f"{lang}", fill="black")
     canvas.itemconfig(guess_word, text=current_card[f"{lang}"], fill="black")
 
-    flip_timer = window.after(3000, func=flip_card)
-
+    #flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
     canvas.itemconfig(canvas_image, image=back_image)
     canvas.itemconfig(language, text="English", fill="white")
     canvas.itemconfig(guess_word, text=current_card["English"], fill="white")
+
+def correct():
+    global to_learn
+    word_generator()
+    to_learn.remove(current_card)
+    print(to_learn)
+
+def incorrect():
+    pass
+    global to_learn
+    word_generator()
+    print(to_learn)
 
 
 # ---------- UI SETUP ---------- #
@@ -48,7 +65,7 @@ window = Tk()
 window.title("Flashy Lingo")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer = window.after(3000, func=flip_card)
+#flip_timer = window.after(3000, func=flip_card)
 
 # ---------- Countdown ---------- #
 
@@ -69,21 +86,20 @@ label = Label(text="Please choose your langauge: ", font=(FONT_NAME, 10, "bold")
 label.config(bg=BACKGROUND_COLOR)
 label.grid(row=0, column=0)
 
-combo = Combobox(state="readonly", values=["Dutch", "French", "German"])
+combo = Combobox(state="readonly", values=["Dutch", "French", "German", "Test"])
 combo.current(0) # Sets a default to Dutch so it can pass in something at the start
 combo.bind("<<ComboboxSelected>>", lambda _ : word_generator()) # Makes the new option change straight away
 combo.grid(row=0, column=1)
 
-print(set_language())
 
 # Buttons #
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=word_generator)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=incorrect)
 wrong_button.grid(column=0, row=2)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=word_generator)
+right_button = Button(image=right_image, highlightthickness=0, command=correct)
 right_button.grid(column=1, row=2)
 
 word_generator()
